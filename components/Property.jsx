4639ca1fe73e5dddadba1a94585ee0c7ccef34f4
jaywonger <48599319+jaywonger@users.noreply.github.com>
2022-01-18 21:34:48 -0800

    added properties to jsx

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,9 +6,15 @@ import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 
+import DefaultImage from '../assets/images/house.jpeg'
+
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID  } }) => (
     <Link href={`/property/${externalID}`} passHref>
-        {title}
+        <Flex flexwrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+            <Box>
+                <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house" />
+            </Box>
+        </Flex>
     </Link>
 )
 
