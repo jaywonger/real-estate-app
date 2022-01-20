3f15a8823c13d650d3f171f2c68db6a9d61048a0
jaywonger <48599319+jaywonger@users.noreply.github.com>
2022-01-19 20:16:54 -0800

    fixed for rent layout

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       ></Banner>
 
-      <Flex flexwrap="wrap">
+      <Flex flexWrap='wrap'>
         {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
 
